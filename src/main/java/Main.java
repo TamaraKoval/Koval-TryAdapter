535acc75086383ada96c74f9e81d914ee9ea5805
TamaraKoval <kovalpsy@mail.ru>
2023-03-28 01:24:08 +0300

    Adapter done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,12 @@
 public class Main {
+
+    public static void main(String[] args) {
+        
+        Ints calc = new IntsCalculator();
+        System.out.println(calc.sum(2, 2));
+        System.out.println(calc.sum(10, 22));
+        System.out.println(calc.mult(5, 6));
+        System.out.println(calc.pow(2, 10));
+
+    }
 }
